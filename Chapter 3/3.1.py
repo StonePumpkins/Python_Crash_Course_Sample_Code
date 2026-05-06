e93bd8f180cf5d7c766eb9353ca5e5af6762ee85
StonePumpkins <3988771969@qq.com>
2026-05-06 17:14:43 +0800

    upd

--- Chapter 3/3.1.py
+++ Chapter 3/3.1.py
@@ -3,3 +3,14 @@ import random
 content=["Alice","Bob","CCb"]
 print(content[2])
 print(f"I hate{content[random.randint(0,2)]}")
+content.append("Dick")
+print(f"I love {content[3]}")
+content.insert(1,"Elbert")
+print(content[1])
+del content[0]
+print(content[0])
+
+new=["A","B","C"]
+new.pop()
+new.remove("A")
+print(new)
